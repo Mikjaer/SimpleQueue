b592094e2d5265bd7394f81f0819b23acc5c8b7a
Mikkel Christensen <mikkel@mikjaer.com>
2019-01-30 18:20:24 +0100

    Flash added

--- SimpleQueue.py
+++ SimpleQueue.py
@@ -4,12 +4,25 @@ import daemon
 import syslog
 import time
 
+import setproctitle
+from flask import Flask
+app = Flask(__name__)
+
+# apt-get install python-setproctitle
+# pip install python-daemon flask
+setproctitle.setproctitle("SimpleQueue")
+
+@app.route("/")
+def hello():
+    return "Foobar"
+
 def log(msg):
     """Write message to log file"""
     syslog.openlog("SimpleQueue")
     syslog.syslog(msg)
 
 with daemon.DaemonContext():
+    app.run("127.0.0.1","8080")
     i = 1;
     while True:
         i = i + 1
