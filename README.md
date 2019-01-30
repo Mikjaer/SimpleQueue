a78f11670c931f9765e6ed33fc39b914865baeab
Mikkel Christensen <mikkel@mikjaer.com>
2019-01-30 20:47:45 +0100

    Work in progress

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SimpleQueue
+
+# Todo , https://www.bogotobogo.com/python/Multithread/python_multithreading_Synchronization_Lock_Objects_Acquire_Release.php
--- SimpleQueue.py
+++ SimpleQueue.py
@@ -5,6 +5,29 @@ import daemon
 from daemon import pidfile
 import syslog
 import time
+import pprint
+import configparser
+
+
+class myConfig:
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read("config.ini")
+    
+    def queues(self):
+        self.foobar = "hest";
+        return self.config.sections()
+
+config = myConfig()
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+for key in config.sections():
+    print key
+    print config[key]["run"]
+
+sys.exit(1)
 
 import lockfile
 import setproctitle
@@ -12,13 +35,35 @@ from flask import Flask
 app = Flask(__name__)
 counter = 0
 
+interactive = False
+
+def log(msg):
+    global interactive
+    """Write message to log file"""
+    if interactive:
+        print msg
+    else:
+        syslog.openlog("SimpleQueue")
+        syslog.syslog(msg)
+
+if len(sys.argv) > 1:
+    for arg in sys.argv[1:]:
+        if arg == "-i":
+            interactive = True
+            log("Running interactive")
+
+
+
 # apt-get install python-setproctitle
 # pip install python-daemon flask
+# pip install configparser
+
 setproctitle.setproctitle("SimpleQueue")
 
 def err(errorMessage):
     print "ERROR: " + errorMessage
     sys.exit(1)
+    print "--------------"
 
 @app.route("/", methods = ['GET'])
 def hello():
@@ -29,19 +74,21 @@ def hello():
 
 @app.route("/demo", methods = ['GET'])
 def flash_demo():
-    with open("demo.html", "r") as myfile:
+    with open("/home/mmc/kode/SimpleQueue/demo.html", "r") as myfile:
         return myfile.read();
 
-def log(msg):
-    """Write message to log file"""
-    syslog.openlog("SimpleQueue")
-    syslog.syslog(msg)
+@app.route("/demo", methods = ['PUT'])
+def put_queues():
+    log("Got queue put");
+    return "Thankyou";
 
 def flaskThread():
     app.debug = False
     app.run("127.0.0.1","8080");
 
 def start():
+    global interactive
+
     pidDir = "/var/run"
     pidFilename = "SimpleQueue.pid";
     pidFile = pidDir + "/" + pidFilename
@@ -50,28 +97,33 @@ def start():
 
     if not os.access(pidDir, os.W_OK):
         err("Cannot write to " + pidDir)
-        sys.exit(1)
 
-    if os.path.isfile(pidFile):
+    elif os.path.isfile(pidFile):
         pid = lockfile.pidlockfile.read_pid_from_pidfile(pidFile)
         err("Pid "+pidFile+" file already exists (pid="+str(pid)+"), refusing to overwrite possibly existing instance")
 
-    with daemon.DaemonContext(pidfile=pidfile.TimeoutPIDLockFile(pidFile)):
-        queueThread();
+    else:
+        log("Starting loop")
+        if interactive:
+            queueThread();
+        else:
+            with daemon.DaemonContext(pidfile=pidfile.TimeoutPIDLockFile(pidFile)):
+                queueThread();
 
 def queueThread():
     i = -1;
-
+    c = 1;
     thread.start_new_thread( flaskThread, ())
 
-    log("Starting loop")
     while True:
-        log("Loop started")
+        log("Loop started " + str(c))
+        c = c + 1
         if i != counter:
             log("Counter set to "+str(counter))
             i = counter
         time.sleep(1);
-
+        for key in config.sections():
+            print key
 
 start()
 
