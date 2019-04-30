9b6bbb5716e326f6b7a64dedf7f5e8f6b65666a9
Mikkel Christensen <mikkel@mikjaer.com>
2019-04-30 02:13:44 +0200

    Debian-package-build scripts

--- build/debian/build-release.sh
+++ build/debian/build-release.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-rm simpleschema_*
 mkdir -p build-dir/usr/share/simplequeue
-mkdir -p build-dir/etc
+mkdir -p build-dir/etc/init.d
 mkdir -p build-dir/DEBIAN
 
 pkgbuild=`cat pkgbuild`
@@ -14,9 +13,12 @@ sed -i "s/Version:.*/Version: $version-$build-$pkgbuild/" files/control
 
 cp files/control build-dir/DEBIAN
 cp files/conffiles build-dir/DEBIAN
+cp files/simplequeue build-dir/etc/init.d
 
-cp ../../config.ini build-dir/etc
+cp ../../config.ini build-dir/etc/SimpleQueue.ini
 cp ../../SimpleQueue.py build-dir/usr/share/simplequeue
+cp ../../testJob.py build-dir/usr/share/simplequeue
+cp ../../testJob.php build-dir/usr/share/simplequeue
 
 dpkg-deb --build ./build-dir simpleschema_$version-$build-$pkgbuild.deb
 
